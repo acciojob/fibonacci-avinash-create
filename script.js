5b1390542b858cf38290d7de82a1c258054b8a6e
Avinash Sharma <42712946+avinash-create@users.noreply.github.com>
2024-02-08 20:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	if (num <= 0) {
+        return "Invalid input";
+    } else if (num === 1) {
+        return 0;
+    }
+
+    let fibPrev = 0;
+    let fibCurrent = 1;
+    let fibNext;
+
+    for (let i = 2; i < num; i++) {
+        fibNext = fibPrev + fibCurrent;
+        fibPrev = fibCurrent;
+        fibCurrent = fibNext;
+    }
+
+    return fibNext;
 }
 
 module.exports = fibonacci;
